21487300f91ad010d1d3f9bf1e74d2b35c4442c5
monica835 <monica.dinglasa@student.passerellesnumeriques.org>
2019-10-16 15:28:43 +0800

    10/16/2019

--- Test/src/test/DBConnect.java
+++ Test/src/test/DBConnect.java
@@ -1,40 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package test;
-import java.sql.*;  
-/**
- *
- * @author dinglasamo_sd2082
- */
-public class DBConnect {
-
-
-    public static void main(String args[])
-    {  
-    try
-    {  
-    Class.forName("com.mysql.jdbc.Driver");  
-    Connection con=DriverManager.getConnection(  
-    "jdbc:mysql://localhost:3306/monica","root","");  
-    //here sonoo is database name, root is username and password  
-    Statement stmt=con.createStatement();  
-    String sqlString ="insert into users(name,age,role) values ('monica','20','student')";
-    stmt.executeUpdate(sqlString);
-    
-    
-    ResultSet rs=stmt.executeQuery("select * from users");  
-    while(rs.next())  
-    System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-    con.close();  
-    }
-    catch(Exception e)
-    { 
-        System.out.println(e);
-   }  
-}  
-}  
-
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//
+//package test;
+//import java.sql.*;  
+///**
+// *
+// * @author dinglasamo_sd2082
+// */
+//public class DBConnect {
+//
+//
+//    public static void main(String args[])
+//    {  
+//    try
+//    {  
+//    Class.forName("com.mysql.jdbc.Driver");  
+//    Connection con=DriverManager.getConnection(  
+//    "jdbc:mysql://localhost:3306/monica","root","");  
+//    //here sonoo is database name, root is username and password  
+//    Statement stmt=con.createStatement();  
+//    String sqlString ="insert into users(name,age,role) values ('monica','20','student')";
+//    stmt.executeUpdate(sqlString);
+//    
+//    
+//    ResultSet rs=stmt.executeQuery("select * from users");  
+//    while(rs.next())  
+//    System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+//    con.close();  
+//    }
+//    catch(Exception e)
+//    { 
+//        System.out.println(e);
+//   }  
+//}  
+//}  
+//
